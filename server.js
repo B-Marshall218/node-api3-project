03f923a12b3bdd11ef6123c736f35a0442fdd5d7
Brian <lawmen218@yahoo.com>
2020-07-16 22:14:14 -0400

    fixed start script

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const server = express();
 const helmet = require("helmet");
 
 
-
+server.use(helmet());
 server.use(express.json());
 server.use("/api/users", logger, userRouter)
 server.use(logger);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
